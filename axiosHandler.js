60197facc375a188c2367e7e8fa39de18ea3ce46
LAmollo <lawinyamuga@gmail.com>
2024-07-15 17:52:01 -0700

    axios  file

--- axiosHandler.js
+++ axiosHandler.js
@@ -8,5 +8,17 @@ axios.interceptors.request.use(config => {
     console.error('Request error', error);
     return Promise.reject(error);
   });
+
+  axios.interceptors.response.use(response => {
+    console.log('Request successful', response);
+    document.body.style.cursor = 'default'; // Set cursor to default
+    return response;
+  }, error => {
+    console.error('Response error', error);
+    document.body.style.cursor = 'default'; // Ensure cursor is reset
+    return Promise.reject(error);
+  });
+  
+  export default axios;
   
   
